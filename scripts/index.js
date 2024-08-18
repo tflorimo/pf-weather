ab940727d482a6833ee3601130da47dc4781f63f
Tomas <tflorimo@gmail.com>
2024-08-18 19:31:29 -0300

    update left side weather data according to api response

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
-const locationButton = domSelect(".loc-button");
-const todayInfo = domSelect(".today-info");
-const todayWeatherIcon = domSelect(".today-weather i");
-const todayTemp = domSelect(".weather-temp");
-const daysList = domSelect(".days-list");
-
 const fetchCurrentWeatherData = (location) => {
+    const locationButton = domSelect(".loc-button");
+    const todayInfo = domSelect(".today-info");
+    const todayWeatherIcon = domSelect(".today-weather i");
+    const todayTemp = domSelect(".weather-temp");
+    const daysList = domSelect(".days-list");
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&lang=es`;
+    const locationEl = domSelect('.today-info > div > span');
+    const currentWeatherDescription = domSelect(".today-weather h3");
     fetch(apiUrl)
     .then(response=>response.json())
     .then(data => {
@@ -16,14 +17,24 @@ const fetchCurrentWeatherData = (location) => {
         const isClearNight = !data.current.is_day && currentWeatherCode == 1000;
         const todayWeatherIconCode = isClearNight ? "moon" : weatherIconMap[currentWeatherCode];
 
-        // Also, fetch current datetime
+        // Update weekday, date, and current location
+        todayInfo.querySelector('h2').textContent = new Date().toLocaleDateString('es-AR', {
+            weekday: 'long'
+        });
+        todayInfo.querySelector('span').textContent = new Date().toLocaleDateString('es-AR', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        });
+        locationEl.textContent = data.location.name;
         
-
-
-        // Update UI based on received data
-
+        // Update weather description
+        todayWeatherIcon.className = `bx bx-${todayWeatherIconCode}`;
+        todayTemp.textContent = todayTemperature;
+        currentWeatherDescription.textContent = data.current.condition.text;
+        
 
     })
 }
 
-fetchCurrentWeatherData("Buenos Aires");
+// fetchCurrentWeatherData("Buenos Aires"); 
